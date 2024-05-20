49c57f65a49de62c3555e3471f1413bd8d112e08
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-05-20 17:54:52 +0200

    Add Store & form_data

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Comic;
+use App\Functions\Helper;
 
 class ComicsController extends Controller
 {
@@ -29,7 +30,23 @@ class ComicsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $form_data= $request->all();
+
+        $new_comic= new comic();
+
+        $new_comic->title = $form_data['title'];
+        $new_comic->slug = Helper::generateSlug($new_comic->title, new Comic());
+        $new_comic->description = $form_data['description'];
+        $new_comic->thumb = $form_data['thumb'];
+        $new_comic->price = $form_data['price'];
+        $new_comic->series = $form_data['series'];
+        $new_comic->sale_date = $form_data['sale_date'];
+        $new_comic->type = $form_data['type'];
+        $new_comic->artists = $form_data('artists'); //array come inserirlo?
+        $new_comic->writers = $form_data('writers'); //array come inserirlo?
+
+        $new_comic->save();
+        return redirect()->route('comics.show', $new_comic);
     }
 
     /**
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -10,35 +10,39 @@
             @csrf
                 <div class="mb-3">
                     <label for="title" class="form-label"> Title </label>
-                    <input type="text" class="form-control" id="title">
+                    <input name="title" type="text" class="form-control" id="title">
                 </div>
                 <div class="mb-3">
                     <label for="price" class="form-label"> Price </label>
-                    <input type="text" class="form-control" id="price">
+                    <input name="price" type="text" class="form-control" id="price">
                 </div>
                 <div class="mb-3">
                     <label for="series" class="form-label"> Series </label>
-                    <input type="text" class="form-control" id="series">
+                    <input name="series" type="text" class="form-control" id="series">
+                </div>
+                <div class="mb-3">
+                    <label for="sale_date" class="form-label"> Sale Date </label>
+                    <input name="sale_date" type="text" class="form-control" id="sale_date">
                 </div>
                 <div class="mb-3">
                     <label for="type" class="form-label"> Type </label>
-                    <input type="text" class="form-control" id="type">
+                    <input name="type" type="text" class="form-control" id="type">
                 </div>
                 <div class="mb-3">
                     <label for="artists" class="form-label"> Artists </label>
-                    <input type="text" class="form-control" id="artists">
+                    <input name="artists" type="text" class="form-control" id="artists">
                 </div>
                 <div class="mb-3">
                     <label for="writers" class="form-label"> Writers </label>
-                    <input type="text" class="form-control" id="writers">
+                    <input name="writers" type="text" class="form-control" id="writers">
                 </div>
                 <div class="mb-3">
                     <label for="description" class="form-label"> Description </label>
-                    <input type="textarea" class="form-control" id="description">
+                    <input name="description" type="textarea" class="form-control" id="description">
                 </div>
                 <div class="mb-3">
                     <label for="thumb" class="form-label"> Thumb </label>
-                    <input type="textarea" class="form-control" id="thumb">
+                    <input name="thumb" type="textarea" class="form-control" id="thumb">
                 </div>
             </div>
 
